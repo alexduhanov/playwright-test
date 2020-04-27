3139c26dba49fa2bfa1d7f77c09f847abb8bcfe0
alex duhanov <alex.duhanov@cerner.com>
2020-04-27 17:07:43 -0500

    Added test

--- tests/playwright/example.test.js
+++ tests/playwright/example.test.js
@@ -1,31 +1,42 @@
+const express = require('express');
+const path = require('path');
 const playwright = require('playwright');
 
-let browser;
+jest.setTimeout(30000);
 
 describe('Example playwright setup', () => {
+  let browser;
+
   beforeAll(async () => {
-    browser = await playwright['chromium'].launch({
-      headless: false,
-    });
+    const app = express();
+
+    app.get('/test.html', (req, res) => res.sendFile(path.join(__dirname + '/test.html')));
+
+    httpServer = require('http').createServer(app);
+    httpServer.listen('3000');
+
+    browser = await playwright['chromium'].launch();
   });
 
   afterAll(async () => {
+    httpServer.close();
     await browser.close();
   });
 
-  test('1', async () => {
+  test('Example test', async () => {
     const context = await browser.newContext();
     const page = await context.newPage();
 
-    await page.goto('http://www.example.com');
-    await context.close();
-  });
+    await page.goto('localhost:3000/test.html');
 
-  test('2', async () => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
+    await page.waitForSelector('"Hello World"');
 
-    await page.goto('http://www.example.com');
+    const form = await page.$('form');
+
+    expect(form).not.toBe(null);
+
+    await page.waitForSelector('form');
+
     await context.close();
   });
 });
